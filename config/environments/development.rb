0baaa2631fc609be98c80563babb0cbac031b076
Dave Evans <dave.evans@freeagent.com>
2017-04-18 20:00:18 +0100

    Add www to default host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "reciparrot.com" }
+  config.action_mailer.default_url_options = { host: "www.reciparrot.com" }
   config.action_mailer.delivery_method = :file
   config.action_mailer.perform_caching = false
 
